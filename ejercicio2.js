a411465d26aad5552f75a7465acf0ab1a1a1c58f
zurdozarazaga <zurdozarazaga@gmail.com>
2021-08-26 19:21:26 -0300

    ejercicio 2

---
+++ ejercicio2.js
@@ -0,0 +1,19 @@
+// 18.5 EJERCICIO 5
+// Completar las condiciones de los if del siguiente script para que los mensajes se muestren
+// siempre de forma correcta:
+
+var numero1 = 5;
+var numero2 = 8;
+ 
+if(numero1 < numero2) {
+  console.log("numero1 no es mayor que numero2");
+}
+if(numero2 > 0) {
+  console.log("numero2 es positivo");
+}
+if(numero1>= 0) {
+  console.log("numero1 es negativo o distinto de cero");
+}
+if(numero1 +1 <= numero2) {
+  console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+}
